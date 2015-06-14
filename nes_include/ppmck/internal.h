bc0e1128cbc7c666616799c002633bf707b7ecd9
Takuya SHIOZAKI <tshiozak@bsdclub.org>
2015-06-14 19:43:33 +0900

    review and refactoring for sound_data_read.

--- nes_include/ppmck/internal.h
+++ nes_include/ppmck/internal.h
@@ -216,47 +216,59 @@ sound_data_write:
 ;-------------------------------------------------------------------------------
 ;command read routine
 ;-------------------------------------------------------------------------------
+
+;--------------------
+; sound_data_read : 演奏データの解釈
+;
+; 備考:
+;	XXX:音源非依存な形での共通化
+;
 sound_data_read:
+.next_cmd:
 	ldx	<channel_selx2
 
 	lda	sound_bank,x
 	jsr	change_bank
 
 	lda	[sound_add_low,x]
+
 ;----------
 ;ループ処理1
-loop_program
+.loop_program:
 	cmp	#CMD_LOOP1
-	bne	loop_program2
+	bne	.loop_program2
 	jsr	loop_sub
-	jmp	sound_data_read
+	jmp	.next_cmd
+
 ;----------
 ;ループ処理2(分岐)
-loop_program2
+.loop_program2:
 	cmp	#CMD_LOOP2
-	bne	bank_command		;duty_set
+	bne	.bank_command		;duty_set
 	jsr	loop_sub2
-	jmp	sound_data_read
+	jmp	.next_cmd
+
 ;----------
 ;バンク切り替え
-bank_command
+.bank_command:
 	cmp	#CMD_BANK_SWITCH
-	bne	int_hwenv_command
+	bne	.int_hwenv_command
 	jsr	data_bank_addr
-	jmp	sound_data_read
+	jmp	.next_cmd
+
 ;----------
 ;データエンド設定
-;data_end:
+;.data_end:
 ;	cmp	#CMD_END
-;	bne	duty_set
+;	bne	.int_hwenv_command
 ;	jsr	data_end_sub
-;	jmp	sound_data_read
+;	jmp	.next_cmd
 
 ;----------
 ;ハードエンベロープ
-int_hwenv_command:
+.int_hwenv_command:
 	cmp	#CMD_HWENV
-	bne	slur_command
+	bne	.slur_command
 
 	jsr	sound_data_address
 	lda	effect2_flags,x
@@ -267,25 +279,24 @@ int_hwenv_command:
 	eor	#EFF2_HWENV_MASK
 	sta	register_high,x
 	jsr	sound_data_address
-	jmp	sound_data_read
-
+	jmp	.next_cmd
 
 ;----------
 ;スラー
-slur_command:
+.slur_command:
 	cmp	#CMD_SLUR
-	bne	smooth_command
+	bne	.smooth_command
 	lda	effect2_flags,x
 	ora	#EFF2_SLUR_ENABLE
 	sta	effect2_flags,x
 	jsr	sound_data_address
-	jmp	sound_data_read
+	jmp	.next_cmd
 
 ;----------
 ;スムース
-smooth_command:
+.smooth_command:
 	cmp	#CMD_SMOOTH
-	bne	pitchshift_command
+	bne	.pitchshift_command
 	jsr	sound_data_address
 	lda	[sound_add_low,x]
 	beq	.smooth_off
@@ -300,37 +311,35 @@ smooth_command:
 	sta	effect2_flags,x
 .smooth_fin:
 	jsr	sound_data_address
-	jmp	sound_data_read
+	jmp	.next_cmd
 
 ;----------
 ;ピッチシフト
-pitchshift_command:
+.pitchshift_command:
 	cmp	#CMD_PITCH_SHIFT
-	bne	duty_set
+	bne	.duty_set
 	lda	ps_step,x
 	beq	.ps_setup
 
-	lda	ps_nextnote,x    ; PSコマンドに使用したノートを基準にする
+	lda	ps_nextnote,x		;PSコマンドに使用したノートを基準にする
 	sta	sound_sel,x
 
-.ps_setup
-	jmp	pitchshift_setup
+.ps_setup:
+	jmp	pitchshift_setup	;音長を伴うコマンドなのでこのまま終了
 
 ;----------
 ;音色設定
-duty_set:
+.duty_set:
 	cmp	#CMD_TONE
-	bne	volume_set
+	bne	.volume_set
 	jsr	sound_data_address
 	lda	[sound_add_low,x]	;音色データ読み出し
 	pha
-	bpl	duty_enverope_part	;ヂューティエンベ処理へ
+	bpl	.duty_enverope_part	;ヂューティエンベ処理へ
 
-
-; register_high = 上位4bit、一時退避先として利用
-
-
-duty_select_part:
+	;デューティー比直接指定
+	;register_high = 上位4bitを一時退避先として利用
+.duty_select_part:
 	lda	effect_flag,x
 	and	#~EFF_DUTYENV_ENABLE
 	sta	effect_flag,x		;デューティエンベロープ無効指定
@@ -354,9 +363,10 @@ duty_select_part:
 	ldy	<channel_selx4
 	sta	$4000,y
 	jsr	sound_data_address
-	jmp	sound_data_read
+	jmp	.next_cmd
 
-duty_enverope_part:
+	;デューティーエンベロープの指定
+.duty_enverope_part:
 	lda	effect_flag,x
 	ora	#EFF_DUTYENV_ENABLE
 	sta	effect_flag,x		;デューティエンベロープ有効指定
@@ -373,19 +383,20 @@ duty_enverope_part:
 	lda	dutyenve_table+1,y
 	sta	duty_add_high,x
 	jsr	sound_data_address
-	jmp	sound_data_read
+	jmp	.next_cmd
 
 ;----------
 ;音量設定
-volume_set:
+.volume_set:
 	cmp	#CMD_VOLUME
-	bne	rest_set
+	bne	.rest_set
 	jsr	sound_data_address
 	lda	[sound_add_low,x]
 	sta	temporary
-	bpl	softenve_part		;ソフトエンベ処理へ
+	bpl	.softenve_part		;ソフトエンベ処理へ
 
-volume_part:
+	;音量直接指定
+.volume_part:
 	lda	effect_flag,x
 	and	#~EFF_SOFTENV_ENABLE
 	sta	effect_flag,x		;ソフトエンベ無効指定
@@ -397,108 +408,125 @@ volume_part:
 	ldy	<channel_selx4
 	sta	$4000,y			;ボリューム書き込み
 	jsr	sound_data_address
-	jmp	sound_data_read
+	jmp	.next_cmd
 
-softenve_part:
+	;ソフトエンベロープ処理
+.softenve_part:
 	jsr	volume_sub
-	jmp	sound_data_read
-;----------
-rest_set:
-	cmp	#CMD_REST
-	bne	lfo_set
+	jmp	.next_cmd
 
+;----------
+;休符
+.rest_set:
+	cmp	#CMD_REST
+	bne	.lfo_set
+
+	;休符フラグを立てる
 	lda	rest_flag,x
 	ora	#RESTF_REST
 	sta	rest_flag,x
 
+	;ウェイトを設定
 	jsr	sound_data_address
 	lda	[sound_add_low,x]
 	sta	sound_counter,x
 
 	ldy	<channel_selx4
 	lda	<channel_sel
-	cmp	#$02
-	beq	tri
 
+	cmp	#$02
+	beq	.rest_tri
+
+	;矩形波チャンネル
 	lda	register_high,x
-	ora	#EFF2_HWENV_MASK	; hw_envelope disable
+	ora	#EFF2_HWENV_MASK	;hw_envelope disable
 	sta	$4000,y
 	jsr	sound_data_address
-	rts
-tri:
+	rts				;音長を伴うコマンドなのでこのまま終了
+
+	;三角波チャンネル
+.rest_tri:
 	lda	#$00
 	sta	$4000,y
 	jsr	sound_data_address
-	rts
+	rts				;音長を伴うコマンドなのでこのまま終了
+
 ;----------
-lfo_set:
+;ピッチLFO設定
+.lfo_set:
 	cmp	#CMD_SOFTLFO
-	bne	detune_set
+	bne	.detune_set
 	jsr	lfo_set_sub
-	jmp	sound_data_read
+	jmp	.next_cmd
 
 ;----------
-detune_set:
+;デチューン設定
+.detune_set:
 	cmp	#CMD_DETUNE
-	bne	sweep_set
+	bne	.sweep_set
 	jsr	detune_sub
-	jmp	sound_data_read
+	jmp	.next_cmd
 
 ;----------
-sweep_set:
+;ハードウェアスイープ設定
+.sweep_set:
 	cmp	#CMD_SWEEP
-	bne	pitch_set
+	bne	.pitch_set
 
 	jsr	sound_data_address
 	lda	[sound_add_low,x]
 	ldy	<channel_selx4
 	sta	$4001,y
 	jsr	sound_data_address
-	jmp	sound_data_read
+	jmp	.next_cmd
+
 ;----------
 ;ピッチエンベロープ設定
-pitch_set:
+.pitch_set:
 	cmp	#CMD_PITCHENV
-	bne	arpeggio_set
+	bne	.arpeggio_set
 	jsr	pitch_set_sub
-	jmp	sound_data_read
+	jmp	.next_cmd
 
 ;----------
 ;ノートエンベロープ設定
-arpeggio_set:
+.arpeggio_set:
 	cmp	#CMD_NOTEENV
-	bne	freq_direct_set
+	bne	.freq_direct_set
 	jsr	arpeggio_set_sub
-	jmp	sound_data_read
+	jmp	.next_cmd
 
 ;----------
 ;再生周波数直接設定
-freq_direct_set:
+.freq_direct_set:
 	cmp	#CMD_DIRECT_FREQ
-	bne	y_command_set
+	bne	.y_command_set
 	jsr	direct_freq_sub
-	rts
+	rts				;音長を伴うコマンドなのでこのまま終了
 
 ;----------
 ;ｙコマンド設定
-y_command_set:
+.y_command_set:
 	cmp	#CMD_WRITE_REG
-	bne	wait_set
+	bne	.wait_set
 	jsr	y_sub
-	jmp	sound_data_read
+	jmp	.next_cmd
 
 ;----------
 ;ウェイト設定
-wait_set:
+.wait_set:
 	cmp	#CMD_WAIT
-	bne	oto_set
+	bne	.keyon_set
 	jsr	wait_sub
-	rts
+	rts				;音長を伴うコマンドなのでこのまま終了
+
 ;----------
-oto_set:
-	; XXX:知らないコマンドが来たときの処理はあったほうが良い
+;キーオンコマンド
+.keyon_set:
+	; XXX:知らないコマンドが来たときの処理はあったほうが良いかも
 	sta	sound_sel,x		;処理はまた後で
 
+	;ポルタメントを無効化する
 	lda	#$00
 	sta	ps_step,x
 
@@ -510,13 +538,11 @@ oto_set:
 	jsr	frequency_set		;周波数セットへ
 	lda	effect2_flags,x		;スラーフラグのチェック
 	and	#EFF2_SLUR_ENABLE
-	beq	no_slur
+	beq	.no_slur
 	lda	effect2_flags,x
 	and	#~EFF2_SLUR_ENABLE
 	sta	effect2_flags,x		;スラーフラグのクリア
 	jmp	sound_flag_clear_key_on
 
-no_slur:
-	jmp	effect_init
-
-
+.no_slur:
+	jmp	effect_init		;音長を伴うコマンドなのでこのまま終了
