d13bd7389c86ea22a64d045f5c596cef2469cf85
Takuya SHIOZAKI <tshiozak@bsdclub.org>
2015-07-09 01:24:30 +0900

    remove unnecessary files.

--- src/make_binary.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-
-sh make_compiler.sh clean install 
-sh make_compiler.sh clean 
-sh make_compiler_w32.sh clean install
-sh make_compiler_w32.sh clean 
-
--- src/make_compiler.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-#
-# usage : make_compiler.sh [install]
-#
-
-cd nesasm ; make -f Makefile.unx $*
-cd ../ppmckc ; make -f Makefile.UNIX $*
-
